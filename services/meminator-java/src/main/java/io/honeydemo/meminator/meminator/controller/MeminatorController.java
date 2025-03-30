9274d47c3d1167ff9b2ddf3441190eab335dde8b
JuanMGarciaF <j.garciafernandez2@zurich.com>
2025-03-30 19:09:45 +0000

    Add a Custom Span in the code

--- services/meminator-java/src/main/java/io/honeydemo/meminator/meminator/controller/MeminatorController.java
+++ services/meminator-java/src/main/java/io/honeydemo/meminator/meminator/controller/MeminatorController.java
@@ -20,6 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 
 @RestController
 public class MeminatorController {
@@ -125,6 +126,7 @@ public class MeminatorController {
         }
     }
 
+    @WithSpan
     private int runConvertCommand(File inputFile, String phrase, String outputFilePath) throws InterruptedException, IOException {
 
           //  Span subprocessSpan = GlobalOpenTelemetry.getTracer("pictureController").spanBuilder("convert").startSpan();
